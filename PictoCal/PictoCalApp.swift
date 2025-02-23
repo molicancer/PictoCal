e347c6726b3eab3b1e24974a595fb5e0e5476d47
molicancer <molicancer@gmail.com>
2025-02-23 20:21:42 +0800

    Initial Commit

---
+++ PictoCal/PictoCalApp.swift
@@ -0,0 +1,17 @@
+//
+//  PictoCalApp.swift
+//  PictoCal
+//
+//  Created by Server on 2025/2/23.
+//
+
+import SwiftUI
+
+@main
+struct PictoCalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PictoCalTests/PictoCalTests.swift
@@ -0,0 +1,17 @@
+//
+//  PictoCalTests.swift
+//  PictoCalTests
+//
+//  Created by Server on 2025/2/23.
+//
+
+import Testing
+@testable import PictoCal
+
+struct PictoCalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
